81412a0217b3eab99b7b7af5b2c4dad21aa7960e
Marsha <marshaleonoradunn@gmail.com>
2020-07-10 14:29:25 +0100

    completed calculator frame, with basic css

---
+++ src/components/Calculator.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Calculator.css";
+
+function Calculator() {
+  return (
+    <div className="container">
+      <p className="calc-display">this is the Calculator</p>
+      <div className="button-container">
+        <button className="button-1"></button>
+        <button className="button-1"></button>
+        <button className="button-1"></button>
+        <button className="button-1"></button>
+        <button className="button-1"></button>
+        <button className="button-1"></button>
+        <button className="button-1"></button>
+        <button className="button-1"></button>
+        <button className="button-1"></button>
+        <button className="button-1"></button>
+        <button className="button-1"></button>
+        <button className="button-1"></button>
+        <button className="button-1"></button>
+        <button className="button-1"></button>
+        <button className="button-1"></button>
+        <button className="button-1"></button>
+        <button className="button-1"></button>
+        <button className="button-1"></button>
+        <button className="button-1"></button>
+        <button className="button-1"></button>
+      </div>
+    </div>
+  );
+}
+
+export default Calculator;
